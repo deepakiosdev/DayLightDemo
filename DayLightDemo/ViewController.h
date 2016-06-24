eceec619de9dc889d55c1e3f941dafe62ad08524
dipak <dipak@pftblr-imac-004.local>
2016-06-24 02:04:21 -0600

    Initial Commit

---
+++ DayLightDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DayLightDemo
+//
+//  Created by dipak on 6/24/16.
+//  Copyright © 2016 Dipak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
